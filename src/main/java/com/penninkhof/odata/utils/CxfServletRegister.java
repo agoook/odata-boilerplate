f675dbd2be4dd519480ceb26639bdcbc140921e7
agok <agok@yandex.ru>
2018-02-19 17:15:12 +0700

    Update org.springframework.boot version to 1.5.10

--- src/main/java/com/penninkhof/odata/utils/CxfServletRegister.java
+++ src/main/java/com/penninkhof/odata/utils/CxfServletRegister.java
@@ -1,19 +1,19 @@
 package com.penninkhof.odata.utils;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.apache.cxf.jaxrs.servlet.CXFNonSpringJaxrsServlet;
-import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.apache.cxf.jaxrs.servlet.CXFJaxrsRuntimeDelegateServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class CxfServletRegister {
 	
 	@Bean
 	public ServletRegistrationBean getODataServletRegistrationBean() {
-		ServletRegistrationBean odataServletRegistrationBean = new ServletRegistrationBean(new CXFNonSpringJaxrsServlet(), "/odata.svc/*");
+		ServletRegistrationBean odataServletRegistrationBean = new ServletRegistrationBean(new CXFJaxrsRuntimeDelegateServlet(), "/odata.svc/*");
 		Map<String, String> initParameters = new HashMap<String, String>();
 		initParameters.put("javax.ws.rs.Application", "org.apache.olingo.odata2.core.rest.app.ODataApplication");
 		initParameters.put("org.apache.olingo.odata2.service.factory", "com.penninkhof.odata.utils.JPAServiceFactory");
